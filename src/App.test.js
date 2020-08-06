836b2c3c204c192c0a14831c9db72f6691ae7688
Adrian Lee <adrianlee0118@gmail.com>
2020-08-05 17:19:48 -0700

    added unit and snapshot testing for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import App, { Search } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("has a valid snapshot", () => {
+    const compoent = renderer.create(<App />);
+    const tree = compoent.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
